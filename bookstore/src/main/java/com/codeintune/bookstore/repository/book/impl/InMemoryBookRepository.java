31144d04480df004a688894238cec7c8829d3814
Nicola Rossi <ncl.rss.92@gmail.com>
2025-11-25 22:16:31 +0100

    InMemoryBookRepository InitializingBeanInterface implemented for data mock start

--- bookstore/src/main/java/com/codeintune/bookstore/repository/book/impl/InMemoryBookRepository.java
+++ bookstore/src/main/java/com/codeintune/bookstore/repository/book/impl/InMemoryBookRepository.java
@@ -1,10 +1,15 @@
 package com.codeintune.bookstore.repository.book.impl;
 
+import com.codeintune.bookstore.model.book.Book;
 import com.codeintune.bookstore.model.book.BookRecord;
+import com.codeintune.bookstore.model.book.enums.Availability;
 import com.codeintune.bookstore.repository.book.BookRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -14,7 +19,7 @@ import java.util.Optional;
  */
 @RequiredArgsConstructor
 @Repository
-public class InMemoryBookRepository implements BookRepository {
+public class InMemoryBookRepository implements BookRepository, InitializingBean {
 
     private final Map<Long, BookRecord> bookRecords;
 
@@ -36,5 +41,43 @@ public class InMemoryBookRepository implements BookRepository {
     @Override
     public void deleteById(Long id) {
         bookRecords.remove(id);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Map<Long, BookRecord> bookRecordsStart = new HashMap<>();
+        Book book1 = new Book();
+        book1.setBookId(1L);
+        book1.setAuthor("Lewis Carroll");
+        book1.setIsbn("MOCK_ISBN");
+        book1.setTitle("Alice In Wonderland");
+        book1.setPublisher("MOCK_PUBLISHER");
+
+        Book book2 = new Book();
+        book2.setBookId(2L);
+        book2.setAuthor("Lewis Carroll");
+        book2.setIsbn("MOCK_ISBN");
+        book2.setTitle("Through The Looking Glass");
+        book2.setPublisher("MOCK_PUBLISHER");
+
+
+        BookRecord bookRecord1 = new BookRecord();
+        bookRecord1.setBookRecordId(1L);
+        bookRecord1.setBookData(book1);
+        bookRecord1.setQuantity(1);
+        bookRecord1.setPrice(BigDecimal.valueOf(10.50));
+        bookRecord1.setAvailability(Availability.IN_STOCK);
+
+        BookRecord bookRecord2 = new BookRecord();
+        bookRecord1.setBookRecordId(2L);
+        bookRecord1.setBookData(book2);
+        bookRecord1.setQuantity(1);
+        bookRecord1.setPrice(BigDecimal.valueOf(10.50));
+        bookRecord1.setAvailability(Availability.IN_STOCK);
+
+        bookRecordsStart.put(bookRecord1.getBookRecordId(),bookRecord1);
+        bookRecordsStart.put(bookRecord2.getBookRecordId(),bookRecord2);
+
+        bookRecords.putAll(bookRecordsStart);
     }
 }
